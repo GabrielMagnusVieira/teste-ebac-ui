9bf5fc81694598bbf8d059d64ccbbe9808fb6988
justaus3r <magnus181098@gmail.com>
2024-09-09 20:05:18 -0300

    ultima atulização

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -1,19 +1,29 @@
 ///<reference types="cypress"/>
 
+import produtosPage from "../../support/page-objects/produtos.page";
+
 describe('Funcionalidade: Produtos', () => {
 
     beforeEach(() => {
-        cy.visit('produtos/')
+        produtosPage.visitarUrl()
     });
     
     it('Deve selecionar um produto da lista', () => {
-        cy.get('.products > .row')
-            //.first()
-            //.last()
-            //.eq(0)
-            .contains('Abominable Hoodie')
-            .click()
+        produtosPage.buscarProdutoLista('Abominable Hoodie')
+        cy.get('#tab-title-description > a').should('contain','Descrição')
+    });
 
-            cy.get('#tab-title-description > a').should('contain','Descrição')
+    it('Deve buscar um produto com sucesso', () => {
+        produtosPage.buscarProduto('Abominable Hoodie')
+        
+        
+    });
+
+    it('Deve visitar a pagina do produto', () => {
+        
+    });
+
+    it('Deve adicionar um produto ao carrinho', () => {
+        
     });
 });
