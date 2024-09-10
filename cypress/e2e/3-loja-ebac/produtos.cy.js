5411021d527195fae691fb9d45c24ad5d4cc0d61
justaus3r <magnus181098@gmail.com>
2024-09-10 17:56:39 -0300

    10/09

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -15,15 +15,42 @@ describe('Funcionalidade: Produtos', () => {
 
     it('Deve buscar um produto com sucesso', () => {
         produtosPage.buscarProduto('Abominable Hoodie')
-        
-        
+        cy.get('.product_title').should('contain', 'Abominable Hoodie' )
     });
 
     it('Deve visitar a pagina do produto', () => {
         
+        produtosPage.visitarProduto('Aether Gym Pant')
+        cy.get('.product_title').should('contain', 'Aether Gym Pant')
     });
 
     it('Deve adicionar um produto ao carrinho', () => {
+
+        produtosPage.buscarProduto('Abominable Hoodie')
+        produtosPage.addProdutoCarrinho('M', 'Green', 3)
+
+        cy.get('.woocommerce-message').should('contain', '3 × “Abominable Hoodie” foram adicionados no seu carrinho.')
         
     });
+
+    it.only('Deve adicionar um produto ao carrinho buscando da massa de dados', () => {
+
+        cy.fixture('produtos').then(dados => {
+            
+            produtosPage.buscarProduto(dados[0].nomeProduto)
+            produtosPage.addProdutoCarrinho(
+                dados[0].tamanho,
+                dados[0].cor,
+                dados[0].quantidade )
+    
+            cy.get('.woocommerce-message').should('contain', dados[0].nomeProduto)
+        })
+
+
+
+        
+        
+    });
+
+    
 });
